4e2153f37aa2c4c3eb8f27606332950c0019fc2b
Siddhesh Chavan <savitachavan029@gmail.com>
2025-08-07 16:57:01 +0530

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,21 @@ dotenv.config({
     path: './.env'
 })
 
+import express from 'express';
+const app = express();
+
 import databaseConnection from './db/databaseConnection.js';
 import connectDB from './db/databaseConnection.js';
-connectDB();
+
+connectDB().then(() => {
+    app.listen(process.env.PORT, () => {
+    console.log("Server Running on port", process.env.PORT)
+});
+}).catch((error) => {
+    console.log("server connection failed", error);
+});
+
+
+
+
+
---
+++ backend/src/utils/error.js
@@ -0,0 +1,9 @@
+const errorHandler = (statusCodeFromRes, msg) => {
+    let error = new Error();
+    error.statusCode = statusCodeFromRes;
+    error.message = msg;
+    return error;
+};
+
+export default errorHandler;
+
